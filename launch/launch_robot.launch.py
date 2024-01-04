590bddb49daf8138a50416436a70bc86f3d2d5b7
Lars Hielscher <128815119+TheStick07@users.noreply.github.com>
2024-01-04 18:02:56 +0100

    Update launch_robot.launch.py

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -75,12 +75,12 @@ def generate_launch_description():
         Node(
             package='rplidar_ros',
             executable='rplidar_composition',
-            output='screen',
-            parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-port0',
-                'frame_id': 'laser_frame',
-                'angle_compensate': True,
-                'scan_mode': 'Standard'
-            }]
-        )
+            name='rplidar_node',
+            parameters=[{'channel_type':'serial',
+                         'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-port0',
+                         'serial_baudrate': '115200',
+                         'frame_id': 'laser_frame',
+                         'inverted': False,
+                         'angle_compensate': True}],
+            output='screen'),
     ])
